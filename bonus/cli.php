c1352cbf73a3c957fe8ba382285681ebcc9a40fc
Lorenzo-Arnau <skyline03@hotmail.it>
2021-02-25 15:46:32 +0100

    Cli bonus works

--- bonus/cli.php
+++ bonus/cli.php
@@ -1,3 +1,13 @@
 <?php 
-$name = $argv[1];
-echo 'Ciao '. $name;
+
+// /*
+// per lanciare questo script php
+// aprire il terminale e digitare:
+// php cli.php pippo
+
+// (pippo è l'argomento in posizione 1 dell'array $argv)
+// */
+
+ echo "Ciao e Benvenuto!\r\nSono un simpatico Terminale e mi chiamo Mimmo, è un piacere avere a che fare con te \r\n";
+ echo  $argv[1] . ' ' . $argv[2];
+
