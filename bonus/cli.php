02b85d8854d6b78f4be88c287f146d5d70734185
Lorenzo-Arnau <skyline03@hotmail.it>
2021-02-25 15:53:08 +0100

    update bonus

--- bonus/cli.php
+++ bonus/cli.php
@@ -8,6 +8,8 @@
 // (pippo è l'argomento in posizione 1 dell'array $argv)
 // */
 
- echo "Ciao e Benvenuto!\r\nSono un simpatico Terminale e mi chiamo Mimmo, è un piacere avere a che fare con te \r\n";
+ echo "Ciao e Benvenuto!\r\nSono un simpatico Terminale e mi chiamo Bastet, è un piacere avere a che fare con te \r\n";
  echo  $argv[1] . ' ' . $argv[2];
+ echo "\r\n sembra che tu abbia capito come fare cose assolutamente intili (per ora) con il terminale! i miei più vivi complimenti! ripeti il cmd con un'altro nome e cognome se vuoi,\r\n
+ oppure alla prossima!";
 
